8ebb481352e3a6b89fb6fab7904811ea2e85a7e8
bazhukovsi <1q2w3e4R!@>
2022-03-29 15:24:16 +0300

    Rest CRUD

--- src/main/java/spring/rest/App.java
+++ src/main/java/spring/rest/App.java
@@ -1,5 +1,11 @@
 package spring.rest;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.rest.configuration.MyConfig;
+import spring.rest.entity.Employee;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        List<Employee> allEmployees = communication.getAllEmployees();
+        System.out.println(allEmployees);
+        System.out.println("-----------------------------");
+        Employee employee = communication.getEmployee(2);
+        System.out.println(employee);
+        System.out.println("-----------------------------");
+//        Employee employee1 = new Employee("Sveta", "Sokolova", "Sales", 1200);
+//        communication.saveEmployee(employee1);
+//        Employee employee1 = new Employee("Sveta", "Sokolova", "HR", 1300);
+//        employee1.setId(11);
+//        communication.saveEmployee(employee1);
+//        System.out.println(employee1);
+        communication.deleteEmployee(11);
+
+
     }
 }
--- src/main/java/spring/rest/configuration/MyConfig.java
+++ src/main/java/spring/rest/configuration/MyConfig.java
@@ -1,9 +1,16 @@
 package spring.rest.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @ComponentScan("spring.rest")
 public class MyConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
